3acaf5c275359cc6a5567a0e514c0f09e7be76ce
tianhuo-tongren <1811334612@qq.com>
2023-07-30 11:59:39 +0800

    23_7_30

--- test_7_30/test_7_30/test.c
+++ test_7_30/test_7_30/test.c
@@ -24,4 +24,89 @@
 //
 
 
+#include <stdio.h>
+
+//
+//int main()
+//{
+//	int t = 0;
+//	/*while (printf("*"))
+//	{
+//		t++;
+//		if (t < 3)
+//			break;
+//	}*/
+//
+//	while ('O')
+//	{
+//		t++;
+//		if (t < 3)
+//			break;
+//	}
+//	return 0;
+//}
+
+
+//
+//int BinarySearch(int* nums, int numsLen, int k)
+//{
+//    int left = 0;
+//    int right = numsLen - 1;
+//    int mid = (left + right) / 2;
+//
+//    while (left <= right)
+//    {
+//        if (nums[mid] < k)
+//        {
+//            left = mid + 1;
+//        }
+//        else if (nums[mid] > k)
+//        {
+//            right = mid - 1;
+//        }
+//        else if (nums[mid] == k)
+//        {
+//            return mid;
+//        }
+//        mid = (left + right) / 2;
+//    }
+//    return -1;
+//}
+//
+//int GetNumberOfK(int* nums, int numsLen, int k) {
+//    // write code here
+//    int count = 0;
+//    int pos = BinarySearch(nums, numsLen, k);
+//
+//    if (pos != -1)
+//    {
+//        int cur1 = pos;
+//        int cur2 = pos - 1;
+//        while (nums[cur1] == k)
+//        {
+//            count++;
+//            cur1++;
+//        }
+//        while (nums[cur2] == k)
+//        {
+//            count++;
+//            cur2--;
+//        }
+//        return count;
+//    }
+//
+//    return 0;
+//}
+//
+//
+//int main()
+//{
+//    int arr[10] = { 0,1,2,3,4,5,6,7,8,9  };
+//    int ret = BinarySearch(arr, 10, 7);
+//    //int ret = GetNumberOfK(arr, 4, 4);
+//    printf("%d\n", ret);
+//    return 0;
+//}
+
+
 
