646b3033e773e2137ec219b801501ec6c6154f77
tianhuo-tongren <1811334612@qq.com>
2023-07-31 17:29:59 +0800

    23_7_31 链表

--- test_7_31/test_7_31/test.c
+++ test_7_31/test_7_31/test.c
@@ -78,3 +78,103 @@
 //}
 
 
+//struct ListNode* reverseList(struct ListNode* head) {
+//    struct ListNode* n1 = NULL;
+//    struct ListNode* n2 = head;
+//    struct ListNode* n3 = NULL;
+//
+//    while (n2)
+//    {
+//        n3 = n2->next;
+//        n2->next = n1;
+//        n1 = n2;
+//        n2 = n3;
+//    }
+//    return n1;
+//}
+
+
+//struct ListNode* middleNode(struct ListNode* head) {
+//    struct ListNode* slow = head;
+//    struct ListNode* fast = head;
+//
+//    while (fast && fast->next)
+//    {
+//        slow = slow->next;
+//        fast = fast->next->next;
+//    }
+//    return slow;
+//}
+
+
+//struct ListNode* FindKthToTail(struct ListNode* pListHead, int k) {
+//    // write code here
+//    if (pListHead == NULL || k == 0)
+//        return NULL;
+//    struct ListNode* slow = pListHead;
+//    struct ListNode* fast = pListHead;
+//
+//    while (k - 1 > 0)
+//    {
+//        fast = fast->next;
+//        k--;
+//    }
+//    if (fast == NULL)
+//        return NULL;
+//
+//    while (fast->next)
+//    {
+//        slow = slow->next;
+//        fast = fast->next;
+//    }
+//    return slow;
+//}
+
+
+//struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+//    if (list1 == NULL)
+//        return list2;
+//    if (list2 == NULL)
+//        return list1;
+//
+//    struct ListNode* newnode = NULL;
+//    struct ListNode* tail = NULL;
+//    while (list1 && list2)
+//    {
+//        if (tail == NULL)
+//        {
+//            if (list1->val <= list2->val)
+//            {
+//                newnode = tail = list1;
+//                list1 = list1->next;
+//            }
+//            else
+//            {
+//                newnode = tail = list2;
+//                list2 = list2->next;
+//            }
+//        }
+//        else
+//        {
+//            if (list1->val <= list2->val)
+//            {
+//                tail->next = list1;
+//                tail = tail->next;
+//                list1 = list1->next;
+//            }
+//            else
+//            {
+//                tail->next = list2;
+//                tail = tail->next;
+//                list2 = list2->next;
+//            }
+//        }
+//    }
+//    if (list1)
+//        tail->next = list1;
+//    if (list2)
+//        tail->next = list2;
+//    return newnode;
+//}
+
+
