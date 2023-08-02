5a5b6e1f3a178bdeeeb5e9b156db28462a6ad56c
tianhuo-tongren <1811334612@qq.com>
2023-08-02 19:28:42 +0800

    23_8_2 leetcode

---
+++ test_8_2/test_8_2/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//struct ListNode* reverseList(struct ListNode* head) {
+//    struct ListNode* newhead = NULL;
+//    struct ListNode* cur = head;
+//
+//    while (cur)
+//    {
+//        struct ListNode* next = cur->next;
+//
+//        cur->next = newhead;
+//        newhead = cur;
+//        cur = next;
+//    }
+//    return newhead;
+//}
+
+
