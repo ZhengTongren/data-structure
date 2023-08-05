bb10fb694b11d23670ca1f13230b7f9ee839d395
tianhuo-tongren <1811334612@qq.com>
2023-08-05 12:58:18 +0800

    23_8_5 复制带随机指针的链表

--- test_8_5/test_8_5/test.c
+++ test_8_5/test_8_5/test.c
@@ -25,16 +25,16 @@
 //}
 
 
-#include <stdio.h>
+//#include <stdio.h>
 
 
-struct Node
-{
-    int val;
-    struct Node* next;
-    struct Node* random;
-};
-
+//struct Node
+//{
+//    int val;
+//    struct Node* next;
+//    struct Node* random;
+//};
+//
 
 //struct Node* BuyLTNode(x)
 //{
@@ -193,6 +193,154 @@ struct Node
 //    NodePushBack(&plist, 20);
 //    NodePushBack(&plist, 30);
 //    NodePrint(&plist);
+//    return 0;
+//}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node
+{
+    int val;
+    struct Node* next;
+    struct Node* random;
+};
+
+
+//struct Node* BuyNode(int x)
+//{
+//    struct Node* newnode = (struct Node*)malloc(sizeof(struct Node));
+//    newnode->val = x;
+//    newnode->next = NULL;
+//    newnode->random = NULL;
+//    return newnode;
+//}
+//
+//void NodePushBack(struct Node** pphead, int x)
+//{
+//    struct Node* cur = *pphead;
+//    struct Node* newnode = BuyNode(x);
+//    if (cur == NULL)
+//    {
+//        *pphead = newnode;
+//    }
+//    else
+//    {
+//        while (cur->next)
+//        {
+//            cur = cur->next;
+//        }
+//        cur->next = newnode;
+//    }
+//}
+//
+//
+//void NodePrint(struct Node* phead)
+//{
+//    struct Node* cur = phead;
+//    while (cur)
+//    {
+//        printf("%d->", cur->val);
+//        cur = cur->next;
+//    }
+//    printf("NULL\n");
+//}
+//
+//int main()
+//{
+//    struct Node* plist = NULL;
+//    NodePushBack(&plist, 10);
+//    NodePushBack(&plist, 10);
+//    NodePrint(plist);
+//    return 0;
+//}
+
+
+//struct Node* BuyNode(int x)
+//{
+//    struct Node* newnode = (struct Node*)malloc(sizeof(struct Node));
+//    newnode->val = x;
+//    newnode->next = NULL;
+//    newnode->random = NULL;
+//    return newnode;
+//}
+//
+//void NodePushBack(struct Node** pphead, int x)
+//{
+//    struct Node* cur = *pphead;
+//    struct Node* newnode = BuyNode(x);
+//    if (*pphead == NULL)
+//    {
+//        *pphead = newnode;
+//    }
+//    else
+//    {
+//        while (cur->next)
+//        {
+//            cur = cur->next;
+//        }
+//        cur->next = newnode;
+//    }
+//}
+//
+//struct Node* copyRandomList(struct Node* head) {
+//    struct Node* cur = head;
+//    struct Node* copyhead, * cur2;
+//    copyhead = cur2 = NULL;
+//
+//    while (cur)
+//    {
+//        NodePushBack(&copyhead, cur->val);
+//        cur = cur->next;
+//    }
+//    cur = head, cur2 = copyhead;
+//    while (cur)
+//    {
+//        struct Node* random = head;
+//        struct Node* random2 = copyhead;
+//        int count = 0;
+//        while (random != cur->random)
+//        {
+//            random = random->next;
+//            count++;
+//        }
+//        while (count--)
+//        {
+//            random2 = random2->next;
+//        }
+//        cur2->random = random2;
+//
+//        cur = cur->next;
+//        cur2 = cur2->next;
+//    }
+//    return copyhead;
+//}
+//
+//
+//int main()
+//{
+//    struct Node* n1, * n2, * n3, * n4, * n5;
+//    n1 = n2 = n3 = n4 = n5 = NULL;
+//    NodePushBack(&n1, 7);
+//    NodePushBack(&n2, 13);
+//    NodePushBack(&n3, 11);
+//    NodePushBack(&n4, 10);
+//    NodePushBack(&n5, 1);
+//    n1->next = n2;
+//    n2->next = n3;
+//    n3->next = n4;
+//    n4->next = n5;
+//    n5->next = NULL;
+//
+//    n1->random = NULL;
+//    n2->random = n1;
+//    n3->random = n5;
+//    n4->random = n3;
+//    n5->random = n1;
+//
+//    struct Node* ptr = copyRandomList(n1);
+//    NodePrint
 //    return 0;
 //}
 
