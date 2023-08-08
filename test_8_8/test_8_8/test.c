550a42cd3804f90df71f9412e552c9f970232f0e
tianhuo-tongren <1811334612@qq.com>
2023-08-08 13:22:37 +0800

    23_8_8 有效的括号leetcode 补充

--- test_8_8/test_8_8/test.c
+++ test_8_8/test_8_8/test.c
@@ -1,41 +1,49 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-//bool isValid(char* s) {
-//    ST st;
-//    STInit(&st);
-//
-//    while (*s)
-//    {
-//        if (*s == '{' || *s == '[' || *s == '(')
-//        {
-//            STPush(&st, *s);
-//        }
-//        else
-//        {
-//            char val = *s;
-//            // 没有匹配的左括号
-//            if (STEmpty(&st))
-//            {
-//                return false;
-//            }
-//
-//            char top = STTop(&st);
-//            STPop(&st);
-//            // 不匹配
-//            if (*s == ']' && top != '['
-//                || *s == '}' && top != '{'
-//                || *s == ')' && top != '(')
-//            {
-//                return false;
-//            }
-//        }
-//        s++;
-//    }
-//
-//    bool ret = STEmpty(&st);
-//
-//    STDestroy(&st);
-//    return ret;
-//}
+bool isValid(char* s) {
+    ST st;
+    STInit(&st);
+
+    while (*s)
+    {
+        if (*s == '{' || *s == '[' || *s == '(')
+        {
+            STPush(&st, *s);
+        }
+        else
+        {
+            char val = *s;
+            // 没有匹配的左括号
+            /*if (STEmpty(&st))
+            {
+                return false;
+            }*/
+            if (STEmpty(&st))
+            {
+                // 避免 {}[] )
+                // STPop只是ps->top--, 并不会把原数据清除
+                // st 中 最后保存了 ]
+                STDestroy(&st);
+                return false;
+            }
+
+            char top = STTop(&st);
+            STPop(&st);
+            // 不匹配
+            if (*s == ']' && top != '['
+                || *s == '}' && top != '{'
+                || *s == ')' && top != '(')
+            {
+                return false;
+            }
+        }
+        s++;
+    }
+
+    bool ret = STEmpty(&st);
+
+    STDestroy(&st);
+    return ret;
+}
 
 
