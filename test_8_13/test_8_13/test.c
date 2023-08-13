5872bacf2674779030ccccfb4e16a62eeabea647
tianhuo-tongren <1811334612@qq.com>
2023-08-13 23:06:56 +0800

    23_8_13 作业

---
+++ test_8_13/test_8_13/test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//void bubble_sort(char* arr, int len)
+//{
+//    for (int i = 0; i < len - 1; i++)
+//    {
+//        for (int j = 0; j < len - i - 1; j++)
+//        {
+//            if (arr[j] > arr[j + 1])
+//            {
+//                char tmp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = tmp;
+//            }
+//        }
+//    }
+//}
+//
+//int main()
+//{
+//    char arr[1001] = { 0 };
+//
+//    while (scanf("%s", arr) != EOF)
+//    {
+//        int len = strlen(arr);
+//        bubble_sort(arr, len);
+//        printf("%s\n", arr);
+//    }
+//    return 0;
+//}
+
+
