d45e5b5d67b97b9eecb3fd3d1cc1fd9ca5e034f9
tianhuo-tongren <1811334612@qq.com>
2023-08-16 17:04:31 +0800

    23_8_16

---
+++ test_8_16/test_8_16/test.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//int pivotIndex(int* nums, int numsSize) {
+//    int sum = 0;
+//    for (int i = 0; i < numsSize; i++)
+//    {
+//        sum += nums[i];
+//    }
+//
+//    int tmp = 0;
+//    for (int i = 0; i < numsSize; i++)
+//    {
+//
+//        // if(tmp * 2 == sum)//err
+//        //     return i + 1;
+//        if (tmp * 2 == sum - nums[i])
+//            return i;
+//        tmp += nums[i];
+//    }
+//
+//    return -1;
+//}
+
+
