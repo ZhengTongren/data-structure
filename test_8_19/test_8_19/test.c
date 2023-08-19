a6ff6844ea11b163603c42344fe25edf7a827f21
tianhuo-tongren <1811334612@qq.com>
2023-08-19 10:44:34 +0800

    23_8_19 leetcode

--- test_8_19/test_8_19/test.c
+++ test_8_19/test_8_19/test.c
@@ -417,3 +417,151 @@
 //	free(obj);
 //	// obj = NULL;
 //}
+
+
+//typedef int STDataType;
+//
+//typedef struct Stack
+//{
+//	STDataType* a;
+//	int top;
+//	int capacity;
+//}ST;
+//
+//void STInit(ST* ps);
+//void STDestroy(ST* ps);
+//void STPush(ST* ps, STDataType x);
+//void STPop(ST* ps);
+//STDataType STTop(ST* ps);
+//
+//int STSize(ST* ps);
+//bool STEmpty(ST* ps);
+//
+//void STInit(ST* ps)
+//{
+//	assert(ps);
+//
+//	ps->a = NULL;
+//	ps->capacity = 0;
+//	ps->top = 0;
+//}
+//
+//
+//void STDestroy(ST* ps)
+//{
+//	assert(ps);
+//
+//
+//	free(ps->a);
+//	ps->a = NULL;
+//	ps->capacity = ps->top = 0;
+//}
+//
+//
+//void STPush(ST* ps, STDataType x)
+//{
+//	assert(ps);
+//
+//	if (ps->top == ps->capacity)
+//	{
+//		int newCapacity = ps->capacity == 0 ? 4 : ps->capacity * 2;
+//		STDataType* tmp = (STDataType*)realloc(ps->a, sizeof(STDataType) * newCapacity);
+//		if (tmp == NULL)
+//		{
+//			perror("realloc");
+//			exit(-1);
+//		}
+//		ps->a = tmp;
+//		ps->capacity = newCapacity;
+//	}
+//
+//	ps->a[ps->top] = x;
+//	ps->top++;
+//}
+//
+//
+//void STPop(ST* ps)
+//{
+//	assert(ps);
+//
+//	ps->top--;
+//}
+//
+//
+//int STSize(ST* ps)
+//{
+//	assert(ps);
+//
+//	return ps->top;
+//}
+//
+//
+//bool STEmpty(ST* ps)
+//{
+//	assert(ps);
+//
+//	return ps->top == 0;
+//}
+//
+//
+//STDataType STTop(ST* ps)
+//{
+//	assert(ps);
+//	assert(ps->top > 0);
+//	return ps->a[ps->top - 1];
+//}
+//
+//
+//typedef struct {
+//	ST pushst;
+//	ST popst;
+//} MyQueue;
+//
+//
+//MyQueue* myQueueCreate() {
+//	MyQueue* obj = (MyQueue*)malloc(sizeof(MyQueue));
+//
+//	STInit(&obj->pushst);
+//	STInit(&obj->popst);
+//	return obj;
+//}
+//
+//void myQueuePush(MyQueue* obj, int x) {
+//	STPush(&obj->pushst, x);
+//}
+//
+//
+//
+//int myQueuePeek(MyQueue* obj) {
+//	if (STEmpty(&obj->popst))
+//	{
+//		while (!STEmpty(&obj->pushst))
+//		{
+//			STPush(&obj->popst, STTop(&obj->pushst));
+//			STPop(&obj->pushst);
+//		}
+//	}
+//
+//	return STTop(&obj->popst);
+//}
+//
+//int myQueuePop(MyQueue* obj) {
+//	// int front = STTop(&obj->popst);// err
+//	// 无法保证 popst 中一定有元素
+//	int front = myQueuePeek(obj);
+//	STPop(&obj->popst);
+//	return front;
+//}
+//
+//bool myQueueEmpty(MyQueue* obj) {
+//	return STEmpty(&obj->pushst) && STEmpty(&obj->popst);
+//}
+//
+//void myQueueFree(MyQueue* obj) {
+//	STDestroy(&obj->pushst);
+//	STDestroy(&obj->popst);
+//
+//	free(obj);
+//}
+
+
