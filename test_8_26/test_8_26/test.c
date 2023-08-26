a1cee02b16ce33b2944234fda90d428c49a0bf73
tianhuo-tongren <1811334612@qq.com>
2023-08-26 15:33:51 +0800

    23_8_26

---
+++ test_8_26/test_8_26/test.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main() {
+//    int n = 0;
+//    scanf("%d", &n);
+//    int flag = 0;
+//    for (int i = 2; i <= n; i++)
+//    {
+//        int sum = 0;
+//        for (int j = 1; j < i; j++)
+//        {
+//            if (i % j == 0)
+//            {
+//                sum += j;
+//            }
+//        }
+//        if (sum == i)
+//        {
+//            //printf("%d\n", i);
+//            flag++;
+//        }
+//    }
+//    printf("%d", flag);
+//    return 0;
+//}
+
+
