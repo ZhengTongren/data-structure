3020eac2ceb53bbcb51df13136e7c43279053247
tianhuo-tongren <1811334612@qq.com>
2023-08-29 23:28:54 +0800

    23_8_29 leetcode

--- test_8_29/test_8_29/test.c
+++ test_8_29/test_8_29/test.c
@@ -23,4 +23,57 @@
 //    return 0;
 //}
 
+//
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main() {
+//    int n = 0, m = 0;
+//
+//    while (scanf("%d %d", &n, &m) == 2)
+//    {
+//        double arr[10000] = { 0.0 };
+//        arr[0] = n;
+//        double total = arr[0];
+//        for (int i = 1; i < m; i++)
+//        {
+//            arr[i] = sqrt(arr[i - 1]);
+//            total += arr[i];
+//        }
+//        printf("%.2f\n", total);
+//    }
+//    return 0;
+//}
+
+//
+//int* masterMind(char* solution, char* guess, int* returnSize) {
+//    int* answer = (int*)malloc(sizeof(int) * 2);
+//    answer[0] = answer[1] = 0;
+//    for (int i = 0; i < 4; i++)
+//    {
+//        if (guess[i] == solution[i])
+//        {
+//            answer[0]++;
+//            guess[i] = solution[i] = '1';
+//        }
+//    }
+//    for (int i = 0; i < 4; i++)
+//    {
+//        if (guess[i] != solution[i])
+//        {
+//            for (int j = 0; j < 4; j++)
+//            {
+//                if (guess[j] == solution[i])
+//                {
+//                    answer[1]++;
+//                    guess[j] = solution[i] = '1';
+//                    break;
+//                }
+//            }
+//        }
+//    }
+//    *returnSize = 2;
+//    return answer;
+//}
+
 
