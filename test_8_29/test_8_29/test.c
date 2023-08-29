c607b0762632b97394f303803f7b874632329962
tianhuo-tongren <1811334612@qq.com>
2023-08-29 22:38:59 +0800

    23_8_29

---
+++ test_8_29/test_8_29/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int arr[32] = { 0 };
+//    int mon = 0;
+//    scanf("%d", &mon);
+//
+//
+//    for (int i = 0; i < mon; i++)
+//    {
+//        if (i >= 2)
+//        {
+//            arr[i] = arr[i - 1] + arr[i - 2];
+//        }
+//        else {
+//            arr[i] = 1;
+//        }
+//    }
+//    printf("%d", arr[mon - 1]);
+//    return 0;
+//}
+
+
