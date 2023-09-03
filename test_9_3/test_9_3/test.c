4fe1bb1d09b7a31d9684e9fe3a18a191517c6172
tianhuo-tongren <1811334612@qq.com>
2023-09-03 21:06:11 +0800

    23_9_3

---
+++ test_9_3/test_9_3/test.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int arr[12] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+//	int* p[4];
+//	int i;
+//	for (int i = 0; i < 4; i++)
+//	{
+//		p[i] = &arr[3 * i];
+//	}
+//	for (int i = 0; i < 4; i++)
+//	{
+//		for (int j = 0; j < 3; j++)
+//		{
+//			printf("%d ", p[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char ch[4] = { 0 };
+//	for (int i = 0; i < 4; i++)
+//	{
+//		printf("%p\n", &ch[i]);
+//	}
+//	return 0;
+//}
+
+
