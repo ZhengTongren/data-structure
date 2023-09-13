e5ae98203b70e5b13374df34de7d4050b43b40fb
tianhuo-tongren <1811334612@qq.com>
2023-09-14 02:17:07 +0800

    23_9_14

---
+++ test_9_14/test_9_14/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//typedef int DataType;
+//
+//struct Node
+//{
+//	struct Node* firstchild;
+//	struct Node* nextbrother;
+//	DataType val;
+//};
+
+//
+//typedef int DataType;
+//
+//struct Node
+//{
+//	struct Node* firstchild;
+//	struct Node* nextbrother;
+//	DataType val;
+//};
+
+
